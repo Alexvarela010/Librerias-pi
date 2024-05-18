79e817668693e58a456802cd6b0afae98ca8dec9
Ubuntu <ubuntu@ip-172-31-21-31.us-east-2.compute.internal>
2024-05-18 02:50:53 +0000

    Actualizacion libreria io

--- ova_io/aplicacion/Aplicacion.java
+++ ova_io/aplicacion/Aplicacion.java
@@ -8,9 +8,9 @@ public class Aplicacion {
     }
 
     public Aplicacion() {
-        double[] restricciones = {1.0,4.0,1600.0,30.0,20.0,18000.0};
+        double[] restricciones = {1.0,4.0,1600.0,1.0,30.0,20.0,18000.0,1.0};
         Lineal lin = new Lineal();
-        double[] sol = lin.lineal(3.0, 4.0, restricciones);
+        double[] sol = lin.lineal("max",10.0, 8.0, restricciones);
         for (int i = 0; i < sol.length; i++) {
             System.out.println(sol[i]);
         }
